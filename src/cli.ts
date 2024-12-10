e46e5cac9fdcf3a27d8d4719f738544ce6219ba6
Mrs. Kikta <melissakikta@gmail.com>
2024-12-09 19:28:03 -0500

    README update

--- src/cli.ts
+++ src/cli.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import { QueryResult } from 'pg';
 import { pool, connectToDb } from './connection.js';
 await connectToDb();
-const app = express();
+//const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
